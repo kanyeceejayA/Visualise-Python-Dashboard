a475564c633616e33898025f2534af698baf4a52
kanyeceejayA <33685273+kanyeceejayA@users.noreply.github.com>
2019-05-08 20:16:52 +0300

    changes to second function

--- 2.py
+++ 2.py
@@ -1,23 +1,14 @@
-import os
-from flask import Flask, request, redirect, url_for, send_from_directory, render_template
-
-UPLOAD_FOLDER = 'C:/Flask-Admin-Dashboard/static/img'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
-
+from flask import Flask, request
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
-
-@app.route('/<filename>')
-def uploaded_file(filename):
-    filename = 'http://127.0.0.1:5000/static/img/photo1.png' 
-    #+ filename
-    return render_template('echo.html', filename=filename)
-
-@app.route('/uploads/<filename>')
-def send_file(filename):
-    return send_from_directory(UPLOAD_FOLDER, filename)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+ 
+@app.route("/")
+def hello():
+    return '<form action="/echo" method="POST"><input name="text"><input type="submit" value="Echo"></form>'
+ 
+@app.route("/echo", methods=['POST'])
+def echo():
+    return "You said: " + request.form['text']
+ 
+ 
+if __name__ == "__main__":
+    app.run()
